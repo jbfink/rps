2d93fea268d0c7e87e49b59596e9a5ed4bdb081c
John Fink <jbfink@zamenhof>
2010-06-29 19:08:32 -0400

    Whoops. fixed/expanded some spock lizard logic.

--- adrbox.rb
+++ adrbox.rb
@@ -23,12 +23,18 @@ tiemessage = "You tie!"
 if userchoice == "R" and computerchoice == "P" then puts losemessage
 elsif userchoice == "R" and computerchoice == "S" then puts winmessage
 elsif userchoice == "R" and computerchoice == "R" then puts tiemessage
+elsif userchoice == "R" and computerchoice == "O" then puts losemessage
+elsif userchoice == "R" and computerchoice == "L" then puts winmessage
 elsif userchoice == "P" and computerchoice == "P" then puts tiemessage
 elsif userchoice == "P" and computerchoice == "S" then puts losemessage
 elsif userchoice == "P" and computerchoice == "R" then puts winmessage
+elsif userchoice == "P" and computerchoice == "O" then puts winmessage
+elsif userchoice == "P" and computerchoice == "L" then puts losemessage
 elsif userchoice == "S" and computerchoice == "P" then puts winmessage
 elsif userchoice == "S" and computerchoice == "S" then puts tiemessage
 elsif userchoice == "S" and computerchoice == "R" then puts losemessage
+elsif userchoice == "S" and computerchoice == "O" then puts losemessage
+elsif userchoice == "S" and computerchoice == "L" then puts winmessage
 elsif userchoice == "O" and computerchoice == "O" then puts tiemessage
 elsif userchoice == "L" and computerchoice == "L" then puts tiemessage
 elsif userchoice == "O" and computerchoice == "R" then puts winmessage
@@ -38,6 +44,7 @@ elsif userchoice == "O" and computerchoice == "L" then puts losemessage
 elsif userchoice == "L" and computerchoice == "R" then puts losemessage
 elsif userchoice == "L" and computerchoice == "P" then puts winmessage
 elsif userchoice == "L" and computerchoice == "S" then puts losemessage
+elsif userchoice == "L" and computerchoice == "O" then puts winmessage
 else puts "WTF?"
 end
 end
