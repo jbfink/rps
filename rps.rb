7a99f4cd8c4ced89adcf7261dd02f7aeb490ebec
John Fink <jbfink@zamenhof>
2010-06-29 19:34:47 -0400

    Expanded README, put GPLv3 text on adrbox, rps, sl.

--- rps.rb
+++ rps.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/ruby
 #rock paper scissors!!
 #FIGHT!!!!
-#(an aside: this is a program I wrote to simulate rock paper scissors
-#in Ruby.  I wrote it for fun.  Everybody loves fun! 
-#rps.rb is gpl'd!!!
-#
-#This version of rps.rb is hereby being made available to you
-#under the terms of the GNU General Public License version 3 as published
-#by the Free Software Foundation.
-#
-#rps.rb is distributed in the hope that it will be useful, but
-#WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#General Public License for more details.
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'adrbox'
 
 
