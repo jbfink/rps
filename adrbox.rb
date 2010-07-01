b3ec2c39bcc5ee0357c5267e8302457a22d84464
John Fink <jbfink@zamenhof>
2010-06-30 22:37:08 -0400

    Broken, but getting closer.

--- adrbox.rb
+++ adrbox.rb
@@ -22,18 +22,30 @@ lwin = %w{P O}
 
 
 def Adrbox.rochambo(userchoice, computerchoice)
+rwin = %w{S L}
+pwin = %w{R O}
+swin = %w{P L}
+owin = %w{R S}
+lwin = %w{P O}
+
 losemessage = "You lose!"
 winmessage = "You win!"
 tiemessage = "You tie!"
-if userchoice == "R" and computerchoice == "P" then puts losemessage
-elsif userchoice == "R" and computerchoice == "S" then puts winmessage
-elsif userchoice == "R" and computerchoice == "R" then puts tiemessage
-elsif userchoice == "P" and computerchoice == "P" then puts tiemessage
-elsif userchoice == "P" and computerchoice == "S" then puts losemessage
-elsif userchoice == "P" and computerchoice == "R" then puts winmessage
-elsif userchoice == "S" and computerchoice == "P" then puts winmessage
-elsif userchoice == "S" and computerchoice == "S" then puts tiemessage
-elsif userchoice == "S" and computerchoice == "R" then puts losemessage
+if userchoice == computerchoice then puts tiemessage
+elsif userchoice == "R" then userarray = rwin
+ if computerchoice.include?userarray then puts winmessage
+ else puts losemessage
+end
+elsif userchoice == "P" then userarray = pwin
+if computerchoice.include?userarray then puts winmessage
+ else puts losemessage
+end
+elsif userchoice == "S" then userarray = swin
+if computerchoice.include?userarray then puts winmessage
+ else puts losemessage
+end
+
+
 else puts "WTF?"
 end
 end
