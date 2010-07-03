52bdaa15618cb2702571535c79be451f335bd457
John Fink <jbfink@zamenhof>
2010-07-03 00:00:43 -0400

    Random verbiage cleanup.

--- adrbox.rb
+++ adrbox.rb
@@ -12,8 +12,6 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module Adrbox
-#let's put array stuff here, or try to!
-#wait, I don't *need* loses.  A loss is anything A) not in win AND not equal to guess.  Right?
 # array go *inside* functions or they can't get asked, right? but maybe this would be a good place to mess
 #with mixins/inheritance?
 
