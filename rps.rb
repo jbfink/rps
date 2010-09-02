afba34b47dab90b300375bb05a2cf9a6a2c63724
John Fink <jbfink@zamenhof>
2010-09-02 10:17:27 -0400

    For rvm 1.9.2. Removed backports, explicitly defined path for adrbox.rb.

--- rps.rb
+++ rps.rb
@@ -15,8 +15,8 @@
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'rubygems'
-require 'backports'
-require 'adrbox'
+#require 'backports'
+require './adrbox.rb'
 
 
 
